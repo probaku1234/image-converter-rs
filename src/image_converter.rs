9826e392947b76f063bd360b515499855722376e
Yun Seob Lee <fhzotxldj@gmail.com>
2024-06-09 13:03:20 +0900

    separate app code from main file and format all code
    create new file for app code
    change test name for performance comparison
    auto format by fmt

--- src/image_converter.rs
+++ src/image_converter.rs
@@ -1,12 +1,17 @@
 use std::path::Path;
 
+use crate::app::ImageFormatEnum;
 use image_dds::ddsfile;
 use pathdiff::diff_paths;
 use rayon::prelude::*;
 
-use crate::ImageFormatEnum;
-
-pub fn convert(files: Vec<String>, source_dir: String, output_path: String, output_format: ImageFormatEnum, dds_format: image_dds::ImageFormat) -> bool {
+pub fn convert(
+    files: Vec<String>,
+    source_dir: String,
+    output_path: String,
+    output_format: ImageFormatEnum,
+    dds_format: image_dds::ImageFormat,
+) -> bool {
     return match output_format {
         ImageFormatEnum::DDS => {
             images_to_dds_sequential(files, source_dir, output_path, dds_format).is_ok()
@@ -17,16 +22,26 @@ pub fn convert(files: Vec<String>, source_dir: String, output_path: String, outp
     };
 }
 
-fn images_to_dds_sequential(files: Vec<String>, source_dir: String, output_path: String, dds_format: image_dds::ImageFormat) -> anyhow::Result<()> {
+fn images_to_dds_sequential(
+    files: Vec<String>,
+    source_dir: String,
+    output_path: String,
+    dds_format: image_dds::ImageFormat,
+) -> anyhow::Result<()> {
     // to prevent processing dds->dds, filter out dds files from files
-    let files: Vec<String> = files.iter().filter(|path| !path.ends_with(".dds")).cloned().collect();
+    let files: Vec<String> = files
+        .iter()
+        .filter(|path| !path.ends_with(".dds"))
+        .cloned()
+        .collect();
 
     for path_string in files {
         let cloned_path = path_string.clone();
         // in order to support processing directory recursive, get diff between current path and source path
-        let mut source_relative_path = diff_paths(Path::new(&cloned_path), Path::new(&source_dir.clone())).ok_or_else(||
-            anyhow::anyhow!("Failed to compute relative path for {}", cloned_path)
-        )?;
+        let mut source_relative_path =
+            diff_paths(Path::new(&cloned_path), Path::new(&source_dir.clone())).ok_or_else(
+                || anyhow::anyhow!("Failed to compute relative path for {}", cloned_path),
+            )?;
 
         let image = image_dds::image::open(Path::new(&path_string))?;
         let rgba_image = image.to_rgba8();
@@ -42,56 +57,82 @@ fn images_to_dds_sequential(files: Vec<String>, source_dir: String, output_path:
         let output_path = Path::new(&output_path).join(source_relative_path);
         let mut writer = std::io::BufWriter::new(std::fs::File::create(output_path)?);
         dds.write(&mut writer)?;
-    };
+    }
 
     Ok(())
 }
 
-fn images_to_dds_parallel(files: Vec<String>, source_dir: String, output_path: String, dds_format: image_dds::ImageFormat) -> anyhow::Result<()> {
+fn images_to_dds_parallel(
+    files: Vec<String>,
+    source_dir: String,
+    output_path: String,
+    dds_format: image_dds::ImageFormat,
+) -> anyhow::Result<()> {
     // to prevent processing dds->dds, filter out dds files from files
-    let files: Vec<String> = files.into_iter().filter(|path| !path.ends_with(".dds")).collect();
+    let files: Vec<String> = files
+        .into_iter()
+        .filter(|path| !path.ends_with(".dds"))
+        .collect();
 
     // Use par_chunks to process files in parallel batches
-    files.par_chunks(5).try_for_each(|chunk| -> anyhow::Result<()> {
-        chunk.iter().try_for_each(|path_string| -> anyhow::Result<()> {
-            let cloned_path = path_string.clone();
-            let mut source_relative_path = diff_paths(Path::new(&cloned_path), Path::new(&source_dir)).ok_or_else(||
-                anyhow::anyhow!("Failed to compute relative path for {}", cloned_path)
-            )?;
+    files
+        .par_chunks(5)
+        .try_for_each(|chunk| -> anyhow::Result<()> {
+            chunk
+                .iter()
+                .try_for_each(|path_string| -> anyhow::Result<()> {
+                    let cloned_path = path_string.clone();
+                    let mut source_relative_path = diff_paths(
+                        Path::new(&cloned_path),
+                        Path::new(&source_dir),
+                    )
+                    .ok_or_else(|| {
+                        anyhow::anyhow!("Failed to compute relative path for {}", cloned_path)
+                    })?;
 
-            let image = image_dds::image::open(Path::new(&path_string))?;
-            let rgba_image = image.to_rgba8();
+                    let image = image_dds::image::open(Path::new(&path_string))?;
+                    let rgba_image = image.to_rgba8();
 
-            let dds = image_dds::dds_from_image(
-                &rgba_image,
-                dds_format,
-                image_dds::Quality::Fast,
-                image_dds::Mipmaps::GeneratedAutomatic,
-            )?;
+                    let dds = image_dds::dds_from_image(
+                        &rgba_image,
+                        dds_format,
+                        image_dds::Quality::Fast,
+                        image_dds::Mipmaps::GeneratedAutomatic,
+                    )?;
 
-            source_relative_path.set_extension("dds");
-            let output_path = Path::new(&output_path).join(source_relative_path);
-            let mut writer = std::io::BufWriter::new(std::fs::File::create(output_path)?);
-            dds.write(&mut writer)?;
+                    source_relative_path.set_extension("dds");
+                    let output_path = Path::new(&output_path).join(source_relative_path);
+                    let mut writer = std::io::BufWriter::new(std::fs::File::create(output_path)?);
+                    dds.write(&mut writer)?;
 
-            Ok(())
-        })
-    })?;
+                    Ok(())
+                })
+        })?;
 
     Ok(())
 }
 
-fn dds_to_images_sequential(files: Vec<String>, source_dir: String, output_path: String, output_format: ImageFormatEnum) -> anyhow::Result<()> {
+fn dds_to_images_sequential(
+    files: Vec<String>,
+    source_dir: String,
+    output_path: String,
+    output_format: ImageFormatEnum,
+) -> anyhow::Result<()> {
     // to prevent processing dds->dds, filter out dds files from files
     let sss: &str = output_format.into();
-    let files: Vec<String> = files.iter().filter(|path| !path.ends_with(sss)).cloned().collect();
+    let files: Vec<String> = files
+        .iter()
+        .filter(|path| !path.ends_with(sss))
+        .cloned()
+        .collect();
 
     for path_string in files {
         let cloned_path = path_string.clone();
         // in order to support processing directory recursive, get diff between current path and source path
-        let mut source_relative_path = diff_paths(Path::new(&cloned_path), Path::new(&source_dir.clone())).ok_or_else(||
-            anyhow::anyhow!("Failed to compute relative path for {}", cloned_path)
-        )?;
+        let mut source_relative_path =
+            diff_paths(Path::new(&cloned_path), Path::new(&source_dir.clone())).ok_or_else(
+                || anyhow::anyhow!("Failed to compute relative path for {}", cloned_path),
+            )?;
 
         let mut reader = std::fs::File::open(path_string)?;
         let dds = ddsfile::Dds::read(&mut reader).unwrap();
@@ -108,37 +149,54 @@ fn dds_to_images_sequential(files: Vec<String>, source_dir: String, output_path:
     Ok(())
 }
 
-fn dds_to_images_parallel(files: Vec<String>, source_dir: String, output_path: String, output_format: ImageFormatEnum) -> anyhow::Result<()> {
+fn dds_to_images_parallel(
+    files: Vec<String>,
+    source_dir: String,
+    output_path: String,
+    output_format: ImageFormatEnum,
+) -> anyhow::Result<()> {
     // to prevent processing dds->dds, filter out dds files from files
     let sss: &str = output_format.into();
-    let files: Vec<String> = files.iter().filter(|path| !path.ends_with(sss)).cloned().collect();
+    let files: Vec<String> = files
+        .iter()
+        .filter(|path| !path.ends_with(sss))
+        .cloned()
+        .collect();
 
-    files.par_chunks(5).try_for_each(|chunk| -> anyhow::Result<()> {
-        chunk.iter().try_for_each(|path_string| -> anyhow::Result<()> {
-            let cloned_path = path_string.clone();
-            // in order to support processing directory recursive, get diff between current path and source path
-            let mut source_relative_path = diff_paths(Path::new(&cloned_path), Path::new(&source_dir.clone())).ok_or_else(||
-                anyhow::anyhow!("Failed to compute relative path for {}", cloned_path)
-            )?;
+    files
+        .par_chunks(5)
+        .try_for_each(|chunk| -> anyhow::Result<()> {
+            chunk
+                .iter()
+                .try_for_each(|path_string| -> anyhow::Result<()> {
+                    let cloned_path = path_string.clone();
+                    // in order to support processing directory recursive, get diff between current path and source path
+                    let mut source_relative_path =
+                        diff_paths(Path::new(&cloned_path), Path::new(&source_dir.clone()))
+                            .ok_or_else(|| {
+                                anyhow::anyhow!(
+                                    "Failed to compute relative path for {}",
+                                    cloned_path
+                                )
+                            })?;
 
-            let mut reader = std::fs::File::open(path_string)?;
-            let dds = ddsfile::Dds::read(&mut reader).unwrap();
+                    let mut reader = std::fs::File::open(path_string)?;
+                    let dds = ddsfile::Dds::read(&mut reader).unwrap();
 
-            let image = image_dds::image_from_dds(&dds, 0)?;
+                    let image = image_dds::image_from_dds(&dds, 0)?;
 
-            let sss: &str = output_format.into();
-            source_relative_path.set_extension(sss);
-            let output_path = Path::new(&output_path).join(source_relative_path);
+                    let sss: &str = output_format.into();
+                    source_relative_path.set_extension(sss);
+                    let output_path = Path::new(&output_path).join(source_relative_path);
 
-            image.save(output_path)?;
-            
-            Ok(())
-        })
-    })?;
+                    image.save(output_path)?;
+
+                    Ok(())
+                })
+        })?;
 
     Ok(())
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -156,12 +214,7 @@ mod tests {
         let output_path = "./test_images".to_string();
         let dds_format = image_dds::ImageFormat::BC1RgbaUnorm;
 
-        let convert_result = images_to_dds_sequential(
-            files,
-            source_dir,
-            output_path,
-            dds_format,
-        );
+        let convert_result = images_to_dds_sequential(files, source_dir, output_path, dds_format);
 
         assert!(convert_result.is_ok());
 
@@ -182,12 +235,8 @@ mod tests {
         let output_path = "./test_images".to_string();
         let output_format = ImageFormatEnum::PNG;
 
-        let convert_result = dds_to_images_sequential(
-            files,
-            source_dir,
-            output_path,
-            output_format,
-        );
+        let convert_result =
+            dds_to_images_sequential(files, source_dir, output_path, output_format);
 
         assert!(convert_result.is_ok());
 
@@ -199,8 +248,8 @@ mod tests {
     }
 
     #[test]
-    fn sex() {
-        use std::time::{Duration, Instant};
+    fn test_compare() {
+        use std::time::Instant;
 
         let files = vec![
             "C:\\Users\\fhzot\\Desktop\\test\\o-a_base.png".to_string(),
@@ -231,27 +280,24 @@ mod tests {
         let dds_format = image_dds::ImageFormat::BC1RgbaUnorm;
 
         let start = Instant::now();
-        let convert_result = images_to_dds_sequential(
-            files,
-            source_dir,
-            output_path,
-            dds_format,
-        );
+        let convert_result = images_to_dds_sequential(files, source_dir, output_path, dds_format);
         let duration = start.elapsed();
 
         assert!(convert_result.is_ok());
-        println!("Time elapsed in images_to_dds_sequential() is: {:?}", duration);
+        println!(
+            "Time elapsed in images_to_dds_sequential() is: {:?}",
+            duration
+        );
 
         let start = Instant::now();
-        let convert_result = images_to_dds_parallel(
-            files_clone,
-            source_dir_clone,
-            output_path_clone,
-            dds_format,
-        );
+        let convert_result =
+            images_to_dds_parallel(files_clone, source_dir_clone, output_path_clone, dds_format);
         let duration = start.elapsed();
 
         assert!(convert_result.is_ok());
-        println!("Time elapsed in images_to_dds_parallel() is: {:?}", duration);
+        println!(
+            "Time elapsed in images_to_dds_parallel() is: {:?}",
+            duration
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,280 +1,15 @@
-#![cfg_attr(
-    not(debug_assertions),
-    windows_subsystem = "windows"
-)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use std::path::Path;
-use std::sync::mpsc::{channel, Receiver, Sender, TryRecvError};
-use std::thread;
 
-use eframe::egui;
-use egui::{Color32, RichText};
+use log::LevelFilter;
 use log4rs::append::console::{ConsoleAppender, Target};
 use log4rs::append::file::FileAppender;
 use log4rs::config::{Appender, Config, Root};
 use log4rs::encode::pattern::PatternEncoder;
-use log::error;
-use log::LevelFilter;
-use strum::{EnumString, IntoStaticStr};
-use strum::IntoEnumIterator;
-use walkdir::WalkDir;
 
-use crate::image_converter::convert;
-
+mod app;
 mod image_converter;
-
-#[derive(Debug, PartialEq, Clone, EnumString, IntoStaticStr, Copy)]
-pub enum ImageFormatEnum {
-    #[strum(serialize = "png")]
-    PNG,
-    #[strum(serialize = "dds")]
-    DDS,
-}
-
-// TODO: separate files
-struct MyApp {
-    selected_source_dir: Option<String>,
-    selected_dest_dir: Option<String>,
-    files: Option<Vec<String>>,
-    output_format: ImageFormatEnum,
-    dds_format: image_dds::ImageFormat,
-    selected_row_index: i8,
-    is_window_open: bool,
-    is_convert_success: Option<bool>,
-    tx: Sender<bool>,
-    rx: Receiver<bool>,
-}
-
-
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui_extras::install_image_loaders(ctx);
-
-        if self.is_window_open {
-            egui::Window::new("Modal").open(&mut self.is_window_open).title_bar(false).show(ctx, |ui| {
-                ui.vertical_centered(|ui| {
-                    ui.add(egui::Spinner::new().size(60.0));
-                    ui.label("converting...");
-                });
-            });
-        }
-
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.set_enabled(!self.is_window_open);
-
-            ui.horizontal(|ui| {
-                if ui.button("Select folder").clicked() {
-                    let files = rfd::FileDialog::new()
-                        .set_directory("/")
-                        .pick_folder();
-
-                    if files.is_some() {
-                        self.selected_source_dir = Some(files.unwrap().to_str().unwrap().to_string());
-                        self.files = None;
-                    }
-                }
-
-                ui.label(format!("Source folder: {:?}", self.selected_source_dir));
-            });
-            ui.horizontal(|ui| {
-                if ui.button("Select folder").clicked() {
-                    let files = rfd::FileDialog::new()
-                        .set_directory("/")
-                        .pick_folder();
-
-                    if files.is_some() {
-                        self.selected_dest_dir = Some(files.unwrap().to_str().unwrap().to_string());
-                    }
-                }
-
-                ui.label(format!("Destination folder: {:?}", self.selected_dest_dir));
-            });
-
-            if self.selected_source_dir.is_some() && self.files.is_none() {
-                let selected_source_dir = self.selected_source_dir.clone();
-
-                self.files = Some(WalkDir::new(selected_source_dir.unwrap())
-                    .into_iter()
-                    .filter_map(|file| file.ok())
-                    .filter(|file| file.metadata().unwrap().is_file())
-                    .filter(|file| {
-                        let array: [String; 3] = ["dds".parse().unwrap(), "png".parse().unwrap(), "jpg".parse().unwrap()];
-                        array.contains(&file.path().extension().unwrap_or("None".as_ref()).to_str().unwrap().to_string())
-                    })
-                    .map(|file| String::from(file.path().to_str().unwrap()))
-                    .collect());
-            }
-
-            ui.horizontal(|ui| {
-                egui::ComboBox::from_label("Output Format")
-                    .selected_text(format!("{:?}", self.output_format))
-                    .show_ui(ui, |ui| {
-                        ui.style_mut().wrap = Some(false);
-                        ui.set_min_width(60.0);
-                        ui.selectable_value(&mut self.output_format, ImageFormatEnum::PNG, "PNG");
-                        ui.selectable_value(&mut self.output_format, ImageFormatEnum::DDS, "DDS");
-                    });
-
-                if self.output_format == ImageFormatEnum::DDS {
-                    egui::ComboBox::from_label("DDS Format")
-                        .selected_text(format!("{:?}", self.dds_format))
-                        .show_ui(ui, |ui| {
-                            ui.style_mut().wrap = Some(false);
-                            ui.set_min_width(200.0);
-
-                            for format in image_dds::ImageFormat::iter() {
-                                ui.selectable_value(&mut self.dds_format, format, format.to_string());
-                            }
-                        });
-                }
-            });
-
-            ui.add_enabled_ui(self.files.as_ref().map_or(false, |vec| !vec.is_empty()), |ui| {
-                if ui.button("Convert").clicked() {
-                    self.is_convert_success = None;
-                    self.is_window_open = true;
-
-                    let files = self.files.clone().unwrap();
-                    let selected_source_dir = self.selected_source_dir.clone().unwrap();
-                    let selected_dist_dir = self.selected_dest_dir.clone().unwrap_or(self.selected_source_dir.clone().unwrap());
-                    let output_format = self.output_format.clone();
-                    let dds_format = self.dds_format.clone();
-
-                    let tx = self.tx.clone();
-
-                    thread::spawn(move || {
-                        let result = convert(files, selected_source_dir, selected_dist_dir, output_format, dds_format);
-                        tx.send(result).expect("failed to send result");
-                    });
-                }
-            });
-
-            if self.is_convert_success.is_some() {
-                let is_convert_success = self.is_convert_success.unwrap();
-
-                if is_convert_success {
-                    ui.label(RichText::new("Success!").color(Color32::GREEN));
-                } else {
-                    ui.label(RichText::new("Failed!").color(Color32::RED));
-                }
-            }
-
-            ui.separator();
-
-            ui.with_layout(egui::Layout::left_to_right(egui::Align::Center), |ui| {
-                ui.vertical(|ui| {
-                    self.table_ui(ui);
-                });
-
-                // show image when column selected
-                if self.selected_row_index > -1 {
-                    let files = self.files.clone().unwrap();
-                    let index = self.selected_row_index.clone();
-                    let current_file = &files[index as usize];
-
-                    ui.add(
-                        egui::Image::new(format!("file://{current_file}"))
-                            .rounding(5.0)
-                    );
-                }
-            });
-
-            // Bottom panel
-            egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
-                ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                    ui.label(format!("v{}", &env!("CARGO_PKG_VERSION")));
-                    egui::warn_if_debug_build(ui);
-                    ui.hyperlink_to("Project homepage", env!("CARGO_PKG_HOMEPAGE"));
-                });
-            });
-
-            // get signal when convert is done
-            let result = self.rx.try_recv();
-            match result {
-                Ok(success) => {
-                    self.is_window_open = false;
-                    if success {
-                        self.is_convert_success = Some(true);
-                    } else {
-                        self.is_convert_success = Some(false);
-                    }
-                }
-                Err(e) => {
-                    if e == TryRecvError::Disconnected {
-                        error!("{:?}", e);
-                    }
-                }
-            }
-        });
-    }
-}
-
-impl MyApp {
-    pub fn new(_cc: &eframe::CreationContext) -> Self {
-        let (tx, rx) = channel();
-        Self {
-            selected_source_dir: None,
-            selected_dest_dir: None,
-            files: None,
-            output_format: ImageFormatEnum::PNG,
-            dds_format: image_dds::ImageFormat::BC1RgbaUnorm,
-            selected_row_index: -1,
-            is_window_open: false,
-            is_convert_success: None,
-            tx,
-            rx,
-        }
-    }
-
-    fn table_ui(&mut self, ui: &mut egui::Ui) {
-        use egui_extras::{Column, TableBuilder};
-
-        let available_height = ui.available_height();
-        let table = TableBuilder::new(ui)
-            .column(Column::auto().resizable(true))
-            .column(Column::remainder())
-            .min_scrolled_height(0.0)
-            .max_scroll_height(available_height)
-            .sense(egui::Sense::click())
-            ;
-
-
-        if self.files.is_some() {
-            let files = self.files.clone().unwrap();
-
-            table
-                .header(20.0, |mut header| {
-                    header.col(|ui| {
-                        ui.heading("Images");
-                    });
-                })
-                .body(|mut body| {
-                    for file_path in files {
-                        body.row(30.0, |mut row| {
-                            let row_index = row.index();
-
-                            row.set_selected(self.selected_row_index == row_index as i8);
-                            row.col(|ui| {
-                                ui.label(file_path);
-                            });
-
-                            self.toggle_row_selection(row.index(), &row.response());
-                        });
-                    }
-                });
-        }
-    }
-
-    fn toggle_row_selection(&mut self, row_index: usize, row_response: &egui::Response) {
-        if row_response.clicked() {
-            if self.selected_row_index == -1 || row_index != self.selected_row_index as usize {
-                self.selected_row_index = row_index as i8;
-            } else {
-                self.selected_row_index = -1;
-            }
-        }
-    }
-}
 
 fn init_logging() -> anyhow::Result<()> {
     use directories::UserDirs;
@@ -286,7 +21,9 @@ fn init_logging() -> anyhow::Result<()> {
         let user_dir = user_dir.unwrap();
         if let Some(document_path) = user_dir.document_dir() {
             let logfile = FileAppender::builder()
-                .encoder(Box::new(PatternEncoder::new("{d(%Y-%m-%d %H:%M:%S)} | {({l}):5.5} | {f}:{L} — {m}{n}")))
+                .encoder(Box::new(PatternEncoder::new(
+                    "{d(%Y-%m-%d %H:%M:%S)} | {({l}):5.5} | {f}:{L} — {m}{n}",
+                )))
                 .build(document_path.join(Path::new("image converter rs/output.log")))?;
             appenders.push(Appender::builder().build("logfile", Box::new(logfile)));
         }
@@ -295,12 +32,12 @@ fn init_logging() -> anyhow::Result<()> {
     let stderr = ConsoleAppender::builder().target(Target::Stderr).build();
     appenders.push(Appender::builder().build("stderr", Box::new(stderr)));
 
-    let config = Config::builder()
-        .appenders(appenders)
-        .build(Root::builder()
+    let config = Config::builder().appenders(appenders).build(
+        Root::builder()
             .appender("logfile")
             .appender("stderr")
-            .build(LevelFilter::Debug))?;
+            .build(LevelFilter::Debug),
+    )?;
 
     log4rs::init_config(config)?;
 
@@ -313,6 +50,6 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "Image Converter",
         eframe::NativeOptions::default(),
-        Box::new(|ctx| Box::new(MyApp::new(ctx))),
+        Box::new(|ctx| Box::new(app::ImageConverterApp::new(ctx))),
     )
 }
